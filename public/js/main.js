9163b6eac469280c67338594caa6c98c41bab987
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-13 10:28:48 +0100

    affichage dernière lettre chaque li

--- public/js/main.js
+++ public/js/main.js
@@ -10,5 +10,10 @@ console.log(document.querySelectorAll('#content'));
 // ### 3. Récupère les li dont la class est important avec querySelector et affiche le résultat dans la console
 console.log(document.querySelector('.important'));
 // ### 4. Récupère les li dont la class est important avec querySelectorAll et affiche le résultat dans la console
-// ### 5. Affiche avec l'aide d'un foreach le contenu de chaque li avec la dernière lettre en majuscule 
+console.log(document.querySelectorAll('.important'));
+// ### 5. Affiche avec l'aide d'un foreach le contenu de chaque li avec la dernière lettre en majuscule
+const listItems =  document.querySelectorAll('li');
+listItems.forEach(element => {
+    console.log(element.textContent[element.textContent.length-1].toUpperCase());
+});
 // ### 6. Récupère le p dont la class est "grandParagraphe" de la div id "content", tu dois ici utiliser un getElementById et un getElementsByClassName
